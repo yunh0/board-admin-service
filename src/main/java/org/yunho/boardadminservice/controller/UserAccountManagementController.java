7b4426052cd726f4ba90e4d6d75d4c3abc7b7ff6
YunhoNam <nam100400@naver.com>
2024-07-31 21:13:45 +0900

    #41 - 회원 관리 페이지 구현
    컨트롤러, 서비스, 뷰

--- src/main/java/org/yunho/boardadminservice/controller/UserAccountManagementController.java
+++ src/main/java/org/yunho/boardadminservice/controller/UserAccountManagementController.java
@@ -1,26 +1,48 @@
 package org.yunho.boardadminservice.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.yunho.boardadminservice.dto.response.UserAccountResponse;
+import org.yunho.boardadminservice.service.UserAccountManagementService;
 
+@RequiredArgsConstructor
 @RequestMapping("/management/user-accounts")
 @Controller
 public class UserAccountManagementController {
 
+    private final UserAccountManagementService userAccountManagementService;
+
     @GetMapping
     public String userAccounts(
-            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             Model model,
             HttpServletRequest request
     ) {
+        model.addAttribute(
+                "userAccounts",
+                userAccountManagementService.getUserAccounts().stream().map(UserAccountResponse::from).toList()
+        );
         model.addAttribute("currentUri", request.getRequestURI());
         return "management/user-accounts";
     }
+
+    @ResponseBody
+    @GetMapping("/{userId}")
+    public UserAccountResponse userAccount(@PathVariable String userId) {
+        return UserAccountResponse.from(userAccountManagementService.getUserAccount(userId));
+    }
+
+    @PostMapping("/{userId}")
+    public String deleteUserAccount(@PathVariable String userId) {
+        userAccountManagementService.deleteUserAccount(userId);
+
+        return "redirect:/management/user-accounts";
+    }
+
 
 }
