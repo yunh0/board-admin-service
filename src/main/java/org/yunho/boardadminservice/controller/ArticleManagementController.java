74f75409a7736d6dcc4e7ea5cb1c0b8848619b16
YunhoNam <nam100400@naver.com>
2024-07-31 20:07:50 +0900

    #35 - 게시글 관리 페이지 구현
    컨트롤러, 서비스, 뷰

--- src/main/java/org/yunho/boardadminservice/controller/ArticleManagementController.java
+++ src/main/java/org/yunho/boardadminservice/controller/ArticleManagementController.java
@@ -1,26 +1,47 @@
 package org.yunho.boardadminservice.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.yunho.boardadminservice.dto.response.ArticleResponse;
+import org.yunho.boardadminservice.service.ArticleManagementService;
 
+@RequiredArgsConstructor
 @RequestMapping("/management/articles")
 @Controller
 public class ArticleManagementController {
 
+    private final ArticleManagementService articleManagementService;
+
     @GetMapping
     public String articles(
-            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             Model model,
             HttpServletRequest request
     ) {
         model.addAttribute("currentUri", request.getRequestURI());
+        model.addAttribute(
+                "articles",
+                articleManagementService.getArticles().stream().map(ArticleResponse::withoutContent).toList()
+        );
         return "management/articles";
+    }
+
+    @ResponseBody
+    @GetMapping("/{articleId}")
+    public ArticleResponse article(@PathVariable Long articleId) {
+        return ArticleResponse.withContent(articleManagementService.getArticle(articleId));
+    }
+
+    @PostMapping("/{articleId}")
+    public String deleteArticle(@PathVariable Long articleId) {
+        articleManagementService.deleteArticle(articleId);
+
+        return "redirect:/management/articles";
     }
 
 }
